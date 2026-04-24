051ff3cc6f103b62ae759fe4135bb27585c0d7e6
kumargauravcode <kumargaurav.singh191@gmail.com>
2026-04-25 00:46:29 +0530

    Create sum.py

---
+++ sum.py
@@ -0,0 +1,10 @@
+#integer sum
+a=int(input("enter first  number"))
+b=int(input("enter second nuumber"))
+total =a+b
+print(total)
+#Ffloating sum
+a=float(input("enter first number"))
+b=float(input("enter second number"))
+total=a+b
+print(total)
